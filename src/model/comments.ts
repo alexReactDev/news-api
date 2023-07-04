bc3336541ad5fd7847b1adcdc19a7109c766e5ec
Alexander <alexander.work.mailbox@gmail.com>
2023-07-04 19:41:35 +0600

    Added sort to comments model

--- src/model/comments.ts
+++ src/model/comments.ts
@@ -2,7 +2,7 @@ import db from "./db.js";
 
 class CommentsModel {
 	async getByPost(id: number, limit: number, offset: number) {
-		const comments = (await db.query("SELECT * FROM comments where post_id = $1 LIMIT $2 OFFSET $3;", [id, limit, offset])).rows;
+		const comments = (await db.query("SELECT * FROM comments where post_id = $1 ORDER BY created DESC LIMIT $2 OFFSET $3;", [id, limit, offset])).rows;
 		const total = +(await db.query("SELECT COUNT(*) FROM comments where post_id = $1;", [id])).rows[0].count;
 
 		return {
